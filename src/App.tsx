1e439b236689eb9729c3f13e2ce6f320e98bc7aa
amnaherex <amnaamjad218@gmail.com>
2026-01-15 05:52:39 +0500

    Made Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import Moviecard from "./components/moviecard"
 const App = () => {
   return (
-    <div>
-      <Moviecard/>
+    <div className="h-screen flex w-screen">
+     
+      <Moviecard movie={ {
+        url:"logo.png" , 
+        alt:"Stranger Things", 
+        title:"Stranger Things",
+         date: 2020
+      }} />
     </div>
   )
 }
--- src/components/Moviecard.tsx
+++ src/components/Moviecard.tsx
@@ -1,9 +1,26 @@
 
-const Moviecard = () => {
+const Moviecard = ({movie}) => {
+
+    function fav(){
+alert("movie added to fav")
+    }
+
   return (
-    <div className="">
-      
+    <div id="movie-card" className="flex justify-center items-center bg-orange-100 flex-col ">
+      <div id="movie-poster">
+        <img src={movie.url} alt={movie.title} />
+      </div>
+
+      <div id="overlay">
+        <button onClick={fav} id="fav">❤</button>
+      </div>
+
+      <div id="movie-info">
+        <h3>{movie.title}</h3>
+        <p>{movie.date}</p>
+      </div>
     </div>
+    
   )
 }
 
